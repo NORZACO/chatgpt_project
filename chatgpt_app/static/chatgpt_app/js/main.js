c2ceff1fe4ca1819b763a4b0b86d10e0d451fd37
NORZACO <caspermotive4@gmail.com>
2023-04-07 21:52:16 +0200

    chat box

--- chatgpt_app/static/chatgpt_app/js/main.js
+++ chatgpt_app/static/chatgpt_app/js/main.js
@@ -10,7 +10,70 @@ generateTextBtn.addEventListener('click', () => {
         .then(response => response.json())
         .then(data => {
             const text = data.text;
-            textContainer.innerHTML = text;
+            textContainer.innerHTML += text; // append new text to existing content
+            textContainer.innerHTML += '<br> <hr>'; // add a space between the two texts
+            // <br></br>
+            promptInput.value = ''; // clear the prompt input field
         })
         .catch(error => console.error(error));
+});
+
+
+
+
+
+
+
+function getCurrentTime() {
+    let today = new Date();
+    let hours = today.getHours();
+    let minutes = today.getMinutes();
+    hours = (hours < 10 ? "0" : "") + hours;
+    minutes = (minutes < 10 ? "0" : "") + minutes;
+    return hours + ":" + minutes;
+}
+
+function appendMessage(text, className, avatar) {
+    const chatlogs = document.getElementById("text-container");
+    const chat = document.createElement("div");
+    chat.className = "chat";
+
+    const chatAvatar = document.createElement("div");
+    chatAvatar.className = "chat-avatar";
+    const img = document.createElement("img");
+    img.src = avatar;
+    chatAvatar.appendChild(img);
+
+    const chatMessage = document.createElement("div");
+    chatMessage.className = "chat-message " + className;
+    const p = document.createElement("p");
+    p.textContent = text;
+    chatMessage.appendChild(p);
+
+    const chatTime = document.createElement("div");
+    chatTime.className = "chat-time";
+    chatTime.textContent = getCurrentTime();
+
+    chatMessage.appendChild(chatTime);
+    chat.appendChild(chatAvatar);
+    chat.appendChild(chatMessage);
+
+    chatlogs.appendChild(chat);
+    chatlogs.scrollTop = chatlogs.scrollHeight;
+}
+
+const timestamp = document.querySelector(".chat-time").textContent;
+const question = document.querySelector(".chat-message.customer p").textContent;
+appendMessage(question, "customer", "{% static 'chatgpt_app/img/customer.png' %}");
+
+document.querySelector(".chat-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    const input = document.querySelector("#prompt");
+    const promptText = input.value;
+    input.value = "";
+    if (promptText.trim() === "") {
+        return;
+    }
+
+    appendMessage(promptText, "ai", "{% static 'chatgpt_app/img/ai.png' %}");
 });
--- main.py
+++ main.py
@@ -1,23 +1,56 @@
-import os
-import openai
+# import os
+# import openai
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+# openai.api_key = os.getenv("OPENAI_API_KEY")
 
-def generate_text(prompt):
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=prompt,
-        temperature=0.5,
-        max_tokens=60,
-        top_p=1.0,
-        frequency_penalty=0.5,
-        presence_penalty=0.0,
-        stop=["You:"]
-    )
+# def generate_text(prompt):
+#     response = openai.Completion.create(
+#         model="text-davinci-003",
+#         prompt=prompt,
+#         temperature=0.5,
+#         max_tokens=60,
+#         top_p=1.0,
+#         frequency_penalty=0.5,
+#         presence_penalty=0.0,
+#         stop=["You:"]
+#     )
 
-    return response.choices[0].text.strip()
+#     return response.choices[0].text.strip()
 
-# Example usage
-prompt = "You: What have you been up to?\nFriend: Watching old movies.\nYou: Did you watch anything interesting?\nFriend:"
-generated_text = generate_text(prompt)
-print(generated_text)
+# # Example usage
+# prompt = "You: What have you been up to?\nFriend: Watching old movies.\nYou: Did you watch anything interesting?\nFriend:"
+# generated_text = generate_text(prompt)
+# print(generated_text)
+import torch
+from transformers import GPT2LMHeadModel, GPT2Tokenizer, TextDataset, DataCollatorForLanguageModeling, Trainer, TrainingArguments
+
+# Load the pre-trained GPT-2 model and tokenizer
+model = GPT2LMHeadModel.from_pretrained('gpt2')
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+
+# Load the training data
+dataset = TextDataset(tokenizer=tokenizer, file_path='/path/to/training_data.txt')
+
+# Define the data collator for language modeling
+data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
+
+# Set the training arguments
+training_args = TrainingArguments(
+    output_dir='./results',
+    overwrite_output_dir=True,
+    num_train_epochs=3,
+    per_device_train_batch_size=4,
+    save_steps=10_000,
+    save_total_limit=2,
+)
+
+# Define the Trainer object and start the training process
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=dataset,
+    data_collator=data_collator,
+)
+
+trainer.train()
+
